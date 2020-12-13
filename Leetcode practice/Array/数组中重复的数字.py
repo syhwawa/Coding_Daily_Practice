3c1a4e1effbca155875965779dcec03c1f12f10d
Yuhao <syhwawa@gmail.com>
2020-12-13 22:53:28 +0000

    upload

---
+++ Leetcode practice/Array/数组中重复的数字.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 22:51:49 2020
+
+@author: syhwawa
+找出数组中重复的数字。
+
+
+在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+
+示例 1：
+
+输入：
+[2, 3, 1, 0, 2, 5, 3]
+输出：2 或 3 
+
+"""
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] != i:
+                j = nums[i]
+                if nums[j] != nums[i]:
+                    nums[j], nums[i] = nums[i], nums[j]
+                else:
+                    return nums[i]
+            else:
+                i += 1
+        return -1
+    
