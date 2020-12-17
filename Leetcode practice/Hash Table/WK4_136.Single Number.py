6bff4314ace7edfe92da5892c34e9a34ee87486b
syhwawa <58612648+syhwawa@users.noreply.github.com>
2020-12-17 00:04:04 +0000

    Update WK4_136.Single Number.py

--- Leetcode practice/Hash Table/WK4_136.Single Number.py
+++ Leetcode practice/Hash Table/WK4_136.Single Number.py
@@ -1,3 +1,4 @@
+"""
 Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 Note:
@@ -12,7 +13,7 @@ Example 2:
 
 Input: [4,1,2,1,2]
 Output: 4
-
+"""
 
 ```Python
 Solution 1:
