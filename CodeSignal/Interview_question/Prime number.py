33906dea4b2a0cea0fb409bab8e40c9aa4bd82e0
syhwawa <58612648+syhwawa@users.noreply.github.com>
2020-12-21 23:59:13 +0000

    Create Prime number.py

---
+++ CodeSignal/Interview_question/Prime number.py
@@ -0,0 +1,20 @@
+"""Given a number n, how to determine if it is prime number."""
+
+
+# Python program to display all the prime numbers within an interval
+
+lower = 900
+upper = 1000
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+
+# Follow up: Do it in more efficient way
