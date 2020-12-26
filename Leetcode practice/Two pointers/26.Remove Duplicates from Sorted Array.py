58e0437a2a667e6487c35a468094b945eaf91f40
syhwawa <58612648+syhwawa@users.noreply.github.com>
2020-12-26 20:49:46 +0000

    Update and rename 26.Remove Duplicates from Sorted Array to 26.Remove Duplicates from Sorted Array.py

--- Leetcode practice/Two pointers/26.Remove Duplicates from Sorted Array.py
+++ Leetcode practice/Two pointers/26.Remove Duplicates from Sorted Array.py
@@ -17,7 +17,6 @@ Your function should return length = 5, with the first five elements of nums bei
 
 It doesn't matter what values are set beyond the returned length.
 
-```Python
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         p1, p2= 0, 1
@@ -32,5 +31,21 @@ class Solution:
                 nums[p1 + 1] = nums[p2]
                 p1 += 1
         return p1 + 1
-```
-O(N)
+# Time complexity : O(N)
+# Space complexity: O(1)
+
+首先注意数组是有序的，那么重复的元素一定会相邻。
+
+要求删除重复元素，实际上就是将不重复的元素移到数组的左侧。
+
+考虑用 2 个指针，一个在前记作 p，一个在后记作 q，算法流程如下：
+
+1.比较 p 和 q 位置的元素是否相等。
+
+如果相等，q 后移 1 位
+如果不相等，将 q 位置的元素复制到 p+1 位置上，q 后移 1 位
+重复上述过程，直到 q 等于数组长度。
+
+返回 p + 1，即为新数组长度。
+
+
