d80afdf4df0be9e0d9f0da72266be8f2b6342191
syhwawa <58612648+syhwawa@users.noreply.github.com>
2020-12-29 23:31:15 +0000

    Update WK10_5. Longest Palindromic Substring.py

--- Leetcode practice/Dynamic Programming/WK10_5. Longest Palindromic Substring.py
+++ Leetcode practice/Dynamic Programming/WK10_5. Longest Palindromic Substring.py
@@ -9,9 +9,20 @@ Example 2:
 
 Input: "cbbd"
 Output: "bb"
+    P(i,i) = True
+    P(i, i+1) = Si = Si+1
+    P(i, j) = P(i+1, j-1)^ Si == Sj
+边界情况即为子串长度为 1 或 2 的情况。我们枚举每一种边界情况，并从对应的子串开始不断地向两边扩展。如果两边的字母相同，我们就可以继续扩展，
+
+例如从 P(i+1,j-1)P(i+1,j−1) 扩展到 P(i,j)P(i,j)；如果两边的字母不同，我们就可以停止扩展，因为在这之后的子串都不能是回文串了。
+
+聪明的读者此时应该可以发现，「边界情况」对应的子串实际上就是我们「扩展」出的回文串的「回文中心」。
+
+方法二的本质即为：我们枚举所有的「回文中心」并尝试「扩展」，直到无法扩展为止，此时的回文串长度即为此「回文中心」下的最长回文串长度。我们对所有的长度求出最大值，即可得到最终的答
 
 
 Solution 1: Method based on center line enumeration
+
 class Solution:
     """
     @param s: input string
@@ -97,5 +108,3 @@ class Solution:
                         start = left
                         
         return s[start: start + max_length]
-       
-   
