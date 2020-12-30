9d3bd4585cbf04016415618b4f832a80ddd815a1
syhwawa <58612648+syhwawa@users.noreply.github.com>
2020-12-30 01:50:33 +0000

    Update 647. Palindromic Substrings.py

--- Leetcode practice/Dynamic Programming/647. Palindromic Substrings.py
+++ Leetcode practice/Dynamic Programming/647. Palindromic Substrings.py
@@ -43,4 +43,31 @@ class Solution:
     
     # Time Complexity : O(N**2)
     # Space Complexity : O(1)
-    
+ 
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        dp = [[0] * len(s) for _ in range(len(s))]
+        """
+        属于动态规划的解法，令dp[j][i]表示子串s[j,i]是否是回文串，状态转移如下：
+
+        当i=j时，单个字符肯定是回文串，可以看成奇数回文串的起点
+        当s[i]=s[j]且i-j=1，则dp[j][i]是回文串，可以看成偶数回文串的起点
+        当s[i]=s[j]且dp[j+1][i-1]是回文串，则dp[j][i]也是回文串
+        其他情形都不是回文串
+        其中条件1、2、3是可以合并的：
+
+        当s[i]=s[j]时，dp[j+1][i-1]是回文串或i-j<2，则dp[j][i]也是回文串
+
+        """
+        
+        for i in range(len(s)):
+            for j in range(i + 1):
+                if s[i] == s[j] and (i - j < 2 or dp[j+1][i-1]):
+                    dp[j][i] = 1
+
+                count += dp[j][i]
+        return count
+        # TC: O(N**2)
+        # SC: O(N**2)
