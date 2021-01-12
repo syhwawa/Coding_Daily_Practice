029d572da5c23a5468f0f6529b019991f5572aaa
syhwawa <58612648+syhwawa@users.noreply.github.com>
2021-01-12 22:02:33 +0000

    Update 107. Binary Tree Level Order Traversal II.py

--- Leetcode practice/DFS & BFS/107. Binary Tree Level Order Traversal II.py
+++ Leetcode practice/DFS & BFS/107. Binary Tree Level Order Traversal II.py
@@ -48,3 +48,23 @@ class Solution:
             res.appendleft(current_level)
             
         return res
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        levelOrder = list()
+        if not root:
+            return levelOrder
+        
+        q = collections.deque([root])
+        while q:
+            level = list()
+            size = len(q)
+            for _ in range(size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            levelOrder.append(level)
+
+        return levelOrder[::-1]
