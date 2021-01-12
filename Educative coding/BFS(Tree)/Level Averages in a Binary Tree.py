6c1164154942c9a3243a5d56ffe8709ce72a0037
syhwawa <58612648+syhwawa@users.noreply.github.com>
2021-01-12 22:27:32 +0000

    Create Level Averages in a Binary Tree.py

---
+++ Educative coding/BFS(Tree)/Level Averages in a Binary Tree.py
@@ -0,0 +1,61 @@
+Leetcode 637
+
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+Example 1:
+Input:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Output: [3, 14.5, 11]
+Explanation:
+The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+
+
+from collections import deque
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_level_averages(root):
+  result = []
+  if root is None:
+    return result
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    levelSize = len(queue)
+    levelSum = 0.0
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+      # add the node's value to the running sum
+      levelSum += currentNode.val
+      # insert the children of current node to the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+    # append the current level's average to the result array
+    result.append(levelSum / levelSize)
+
+  return result
+
+
+def main():
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(9)
+  root.left.right = TreeNode(2)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  print("Level averages are: " + str(find_level_averages(root)))
+
+
+main()
