ed6ee6c1b9d949c1e6279a6a3a01dadb73ba841b
syhwawa <58612648+syhwawa@users.noreply.github.com>
2021-01-20 00:51:58 +0000

    Update 2.All Paths for a Sum.py

--- Educative coding/Tree(DFS)/2.All Paths for a Sum.py
+++ Educative coding/Tree(DFS)/2.All Paths for a Sum.py
@@ -2,4 +2,43 @@ Leetcode 113
 Problem Statement #
 Given a binary tree and a number ‘S’, find all paths from root-to-leaf such that the sum of all the node values of each path equals ‘S’.
 
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+def find_paths(root, sum):
+  res = []
+  # TODO: Write your code here
+
+  def dfs(root, sum, res, path):
+    if not root:
+      return
+
+    if not root.left and not root.right:
+      if root.val == sum:
+        res.append(path + [root.val])
+    
+    dfs(root.left, sum - root.val, res, path + [root.val])
+    dfs(root.right, sum - root.val, res, path + [root.val])
+
+  dfs(root, sum, res, [])
+  return res
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  sum = 23
+  print("Tree paths with sum " + str(sum) +
+        ": " + str(find_paths(root, sum)))
+
+main()
+
+
 
