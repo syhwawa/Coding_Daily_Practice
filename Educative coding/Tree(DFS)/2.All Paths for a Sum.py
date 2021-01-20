1d4c734e39d852d9684c12400354d621c86fd13b
syhwawa <58612648+syhwawa@users.noreply.github.com>
2021-01-20 21:28:14 +0000

    Update 2.All Paths for a Sum.py

--- Educative coding/Tree(DFS)/2.All Paths for a Sum.py
+++ Educative coding/Tree(DFS)/2.All Paths for a Sum.py
@@ -41,4 +41,45 @@ def main():
 main()
 
 
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+from collections import deque
+def find_paths(root, sum):
+  res = []
+  queue = deque()
+  queue.append((root, [],0))
+
+  while queue:
+    current_node, path, path_sum = queue.popleft()
+    if not current_node:
+      continue
+    if not current_node.left and not current_node.right:
+      if current_node.val == sum - path_sum:
+        res.append(path + [current_node.val])
+
+    queue.append((current_node.left, path + [current_node.val], path_sum +current_node.val))
+    queue.append((current_node.right, path + [current_node.val], path_sum +current_node.val))
+
+  return res
+
+def main():
+
+  root = TreeNode(12)
+  root.left = TreeNode(7)
+  root.right = TreeNode(1)
+  root.left.left = TreeNode(4)
+  root.right.left = TreeNode(10)
+  root.right.right = TreeNode(5)
+  sum = 23
+  print("Tree paths with sum " + str(sum) +
+        ": " + str(find_paths(root, sum)))
+
+
+main()
+
+
 
