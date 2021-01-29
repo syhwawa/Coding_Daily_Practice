5f8cd001a012af692890c55773b1e7b9ee0cb7ac
syhwawa <58612648+syhwawa@users.noreply.github.com>
2021-01-29 18:46:15 +0000

    Update 2. Return kth to last.py

--- Crack Coding interview/2. Linked List/2. Return kth to last.py
+++ Crack Coding interview/2. Linked List/2. Return kth to last.py
@@ -1,14 +1,33 @@
 # Implement an algorithm to find the kth to last element of a singly linked list.
+Implement an algorithm to find the kth to last element of a singly linked list. Return the value of the element.
 
-def kth_to_last(ll, k):
-    runner = current = ll.head
-    for _ in range(k):
-        if not runner:
-            return None
-        runner = runner.next
+Note: This problem is slightly different from the original one in the book.
 
-    while runner:
-        current = current.next
-        runner = runner.next
+Example:
 
-    return current
+Input:  1->2->3->4->5 和 k = 2
+Output:  4
+Note:
+
+k is always valid.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def kthToLast(self, head: ListNode, k: int) -> int:
+        prev = head
+        cur = head
+        for i in range(k):
+            if not cur:
+                return None
+            cur = cur.next
+
+        while cur:
+            cur = cur.next
+            prev = prev.next
+        
+        return prev.val
