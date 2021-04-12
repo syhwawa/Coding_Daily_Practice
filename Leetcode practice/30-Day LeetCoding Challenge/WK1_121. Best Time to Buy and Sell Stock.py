25221c5c5ab2cabff5a28ed52996c40d0ea27771
Yuhao Sun <58612648+syhwawa@users.noreply.github.com>
2021-04-12 23:46:39 +0100

    Update and rename WK1_121. Best Time to Buy and Sell Stock to WK1_121. Best Time to Buy and Sell Stock.py

--- Leetcode practice/30-Day LeetCoding Challenge/WK1_121. Best Time to Buy and Sell Stock.py
+++ Leetcode practice/30-Day LeetCoding Challenge/WK1_121. Best Time to Buy and Sell Stock.py
@@ -1,3 +1,4 @@
+"""
 Say you have an array for which the ith element is the price of a given stock on day i.
 
 If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
@@ -19,9 +20,9 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 
 1. Define and iterate min price of the list
 2. iterate max_pro
+"""
 
-
-Solution 1：
+# Solution 1：
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
@@ -33,7 +34,7 @@ class Solution:
             
         return max_profit
         
-        
+  
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:  
          if len(prices) <= 1:
